a4d527833c8d20c0374e984a67525b33260c5074
OrkeshKZ <kurashulyorkesh@gmail.com>
2022-06-17 23:30:48 +0600

    Добавлено в модель поле geoposition. Также выведено на темплейт в виде текста

--- hot_app/models.py
+++ hot_app/models.py
@@ -6,6 +6,7 @@ class HotSprings(models.Model):
     name = models.CharField(max_length=100)
     insta = models.TextField()
     website = models.CharField(max_length=50, null=True, blank=True)
+    geoposition = models.CharField(max_length=100, null=True, blank=True)
     
     def __str__(self):
         return self.name
