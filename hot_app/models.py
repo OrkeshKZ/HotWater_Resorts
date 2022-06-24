97828b9062d345cf98c50e541e4b362ccd508dbc
OraKZ <kurashulyorkesh@gmail.com>
2022-06-24 15:55:40 +0600

    added DetailView and Photos

--- hot_app/models.py
+++ hot_app/models.py
@@ -7,6 +7,8 @@ class HotSprings(models.Model):
     insta = models.TextField()
     website = models.CharField(max_length=50, null=True, blank=True)
     geoposition = models.CharField(max_length=100, null=True, blank=True)
+    photo = models.ImageField(upload_to='photos', null=True, blank=True)
+
     
     def __str__(self):
         return self.name
--- hot_app/views.py
+++ hot_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import HotSprings
+from django.views.generic import DetailView
 from django.views.generic import ListView  # django.views.generic хранилище представлений с библиотеки Джанго
 
 # Create your views here.
@@ -8,3 +9,7 @@ class HotSpringsView(ListView):
     model = HotSprings #Указывает из какой модели брать данные!
     template_name = "home.html" #Указывает на какой темплейт выводить данные!
     
+    
+class plaisesDetailView(DetailView):
+    model = HotSprings
+    template_name = 'plaisesDetail.html'
